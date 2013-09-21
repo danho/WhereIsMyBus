c545585bae983cc1daaba13c50fd23778216e86d
D <dannyqho@gmail.com>
2013-09-21 10:55:31 -0400

    added info windows

--- public/map.js
+++ public/map.js
@@ -4,7 +4,7 @@ var markers = new Array();
 function init() {
   var mapOptions = {
     center: new google.maps.LatLng(39.951719, -75.163877),
-    zoom: 14,
+    zoom: 13,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
@@ -31,10 +31,20 @@ function updateMarker(lat, lng, id) {
       } else {
         markers.splice(i, 1);
         addMarker(lat, lng, id);
+        attachWindow(markers[markers.length-1], marker_lat, lat, marker_lng, lng);
         break;
       }
     }
   }
+}
+
+function attachWindow(marker, old_lat, new_lat, old_lng, new_lng) {
+  var contentString = '<div id="content">Old latitude: ' + old_lat + '<br/>New latitude: ' + new_lat + '<br/>Old longitude:' + old_lng + '<br/>New longitude:' + new_lng + '</h4>';
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+  infowindow.open(marker.get('map'), marker);
+  setTimeout(function() { infowindow.close(); }, '7000');
 }
 
 google.maps.event.addDomListener(window, 'load', init);
